b37f76777544754d7444754a6c336fa314f2f825
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-09-15 10:12:36 -0500

    Add project files.

---
+++ 3033_4_lc_0915/Program.cs
@@ -0,0 +1,20 @@
+﻿// Jack Herdejurgen 113436899
+// Class
+
+// Simple: int double char bool
+// Complex: string
+
+using _3033_4_lc_0915;
+using _3033_4_lc_0915.mis3033_herdejurgen;
+
+int age;
+Person p1;
+p1 = new Person();
+age = 20;
+
+
+
+Student stu1 = new Student("Jack",23);
+
+Console.ReadLine();
+
---
+++ 3033_4_lc_0915/Student.cs
@@ -0,0 +1,33 @@
+﻿using _3033_4_lc_0915.mis3033_herdejurgen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3033_4_lc_0915
+{
+    namespace mis3033_herdejurgen
+    {
+        public class Person
+        {
+            public string name { get; set; }
+            public int age { get; set; }
+
+        }
+    }
+
+
+    public class Student : Person
+    {
+        public Student()
+        {
+
+        }
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
